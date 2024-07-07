4bf07fb8d69b2bc555ac8516ca4a74c244c0173b
tonyssmirnov1993 <TonyS33@yandex.ru>
2024-07-07 22:12:58 +0300

    Review_8

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,18 @@
-// место для вашего кода
+#include <iostream>
+#include <string>
+
+#include "input_reader.h"
+#include "stat_reader.h"
+
+using namespace std;
+using namespace transport_catalogue;
+using namespace transport_catalogue::detail;
+
+int main() {
+    TransportCatalogue catalogue;          
+    {
+        InputReader reader;
+        reader.Input(catalogue);              
+    }
+    Output(catalogue); 
+}
