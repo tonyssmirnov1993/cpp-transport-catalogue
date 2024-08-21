a84830635799c8a2fdfb66e7504900d1a5252fc3
tonyssmirnov1993 <TonyS33@yandex.ru>
2024-08-21 22:24:21 +0300

    Sprint_9

--- transport-catalogue/Спринт#9/Тема1#Умные указатели/Урок4#Умный указатель shared_ptr/texture.h
+++ transport-catalogue/Спринт#9/Тема1#Умные указатели/Урок4#Умный указатель shared_ptr/texture.h
@@ -1,24 +1,30 @@
 #pragma once
 #include "common.h"
- 
-#include <iostream>
- 
+
 class Texture {
 public:
     explicit Texture(Image image)
         : image_(std::move(image)) {
+        //size_ = { int(image.size()), int(image[0].size())};
     }
- 
+
     Size GetSize() const {
-        int height = image_.size();
+        // Заглушка. Реализуйте метод самостоятельно
+        if (image_.empty()) {
+            return { 0, 0 };
+        }
+        int hight = image_.size();
         int width = image_.back().size();
-        return {width, height};
+        return {width, hight};
     }
- 
+
     char GetPixelColor(Point p) const {
-        return image_[p.y][p.x];
+        //(void) p;
+        
+       return image_[p.y][p.x];
     }
- 
+
 private:
+    //Size size_ = {};
     Image image_;
 };
---
+++ transport-catalogue/Спринт#9/Тема1#Умные указатели/Урок6#Copy on write/tile.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <array>
+#include <cassert>
+#include <vector>
+#include "geom.h"
+#include <string>
+class Tile {
+public:
+    // Размер тайла 8*8 пикселей.
+    constexpr static int SIZE = 8;
+ 
+    // Конструктор по умолчанию. Заполняет тайл указанным цветом.
+    Tile(char color = ' ') noexcept {
+        /* Реализуйте недостающий код самостоятельно. */
+        pixels_ = std::vector<std::vector<char>>(SIZE, std::vector<char>(SIZE, color));
+        //pixels_ = std::vector<std::vector<std::vector<char>>>(SIZE, std::vector<std::vector<char>>(SIZE, std::vector<char>(SIZE, ' ')));
+        
+        // -------------- не удалять ------------
+        assert(instance_count_ >= 0);
+        ++instance_count_;  // Увеличиваем счётчик тайлов (для целей тестирования).
+        // -------------- не удалять ------------
+    }
+ 
+    Tile(const Tile& other) {
+        /* Реализуйте недостающий код самостоятельно. */
+        pixels_ = other.pixels_;
+        // -------------- не удалять ------------
+        assert(instance_count_ >= 0);
+        ++instance_count_;  // Увеличиваем счётчик тайлов (для целей тестирования).
+        // -------------- не удалять ------------
+    }
+ 
+    ~Tile() {
+        // -------------- Не удалять ------------
+        --instance_count_;  // Уменьшаем счётчик тайлов.
+        assert(instance_count_ >= 0);
+        // -------------- не удалять ------------
+    }
+ 
+    /**
+     * Изменяет цвет пикселя тайла.
+     * Если координаты выходят за пределы тайла, метод ничего не делает.
+     */
+    void SetPixel(Point p, char color) noexcept {
+        /* Реализуйте недостающий код самостоятельно. */
+ 
+        if (p.y >= 0 && p.y <= SIZE && p.x >= 0 && p.x <= SIZE) {
+            pixels_[p.y][p.x] = color;
+        }
+    }
+ 
+    /**
+     * Возвращает цвет пикселя. Если координаты выходят за пределы тайла, возвращается пробел.
+     */
+ 
+ 
+    char GetPixel(Point p) const noexcept {
+        /* Реализуйте недостающий функционал самостоятельно. */
+        if (p.y >= 0 && p.y <= SIZE && p.x >= 0 && p.x <= SIZE) {
+            return pixels_[p.y][p.x];
+        }
+        return ' ';
+ 
+    }
+ 
+    // Возвращает количество экземпляра класса Tile в программе.
+    static int GetInstanceCount() noexcept {
+        // -------------- не удалять ------------
+        return instance_count_;
+        // -------------- не удалять ------------
+    }
+ 
+private:
+    // -------------- не удалять ------------
+    inline static int instance_count_ = 0;
+    // -------------- не удалять ------------
+ 
+    /* Разместите здесь поля для хранения пикселей тайла. */
+    std::vector<std::vector<char>> pixels_;
+ 
+};
